76dd44b60196c0af37ee0bc06fc2362529ca3d0f
scliff6262 <sclifford6262@gmail.com>
2019-05-03 00:28:48 -0400

    User can create products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,41 @@
 class ProductsController < ApplicationController
+
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    redirect_to product_path @product
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.assign_attributes(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render "/products/edit"
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :limit)
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   has_many :inventories
   has_many :machines, through: :inventories
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :limit, presence: true
 end
